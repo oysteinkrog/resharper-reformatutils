b0a095d690ce242f05fa109e029dfa5eeb4c2c37
Øystein Krog <oystein.krog@gmail.com>
2013-10-31 20:05:27 +0100

    Add reformat struct context action, bump version to 1.5.0

---
+++ src/ReformatUtils/ContextActions/ReformatStruct.cs
@@ -0,0 +1,29 @@
+﻿using JetBrains.ReSharper.Feature.Services.Bulbs;
+using JetBrains.ReSharper.Feature.Services.CSharp.Bulbs;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.Tree;
+using ReformatUtils.ContextActions.Base;
+
+namespace ReformatUtils.ContextActions
+{
+    [ContextAction(Name = "ReformatStruct", Description = Description, Group = "C#")]
+    public class ReformatStruct : ReformatTreeNodeContextActionBase<IStructDeclaration>
+    {
+        private const string Description = "Reformat struct";
+
+        public ReformatStruct(ICSharpContextActionDataProvider provider)
+            : base(provider)
+        {
+        }
+
+        public override string Text
+        {
+            get { return Description; }
+        }
+
+        protected override ITreeNode GetNodeToFormat(IStructDeclaration element)
+        {
+            return element.Body;
+        }
+    }
+}
--- src/ReformatUtils/Properties/AssemblyInfo.cs
+++ src/ReformatUtils/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using JetBrains.Application.PluginSupport;
 [assembly: AssemblyCopyright("Copyright © Øystein Krog, 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.4.1.*")]
+[assembly: AssemblyVersion("1.5.0.*")]
 [assembly: ActionsXml("ReformatUtils.Actions.xml")]
 
 // The following information is displayed by ReSharper in the Plugins dialog
